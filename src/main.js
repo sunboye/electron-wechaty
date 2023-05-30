ed1f720f9bb1254392740cc394139262e816025f
yangss <1332551701@qq.com>
2023-05-30 18:06:37 +0800

    项目结构调整

--- src/main.js
+++ src/main.js
@@ -3,13 +3,13 @@
  * @Position: 
  * @Date: 2023-05-29 18:17:08
  * @LastEditors: yangss
- * @LastEditTime: 2023-05-30 13:51:54
+ * @LastEditTime: 2023-05-30 17:44:31
  * @FilePath: \electron-wechaty\src\main.js
  */
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import App from './webapps/App.vue'
+import router from './webapps/router'
+import store from './webapps/store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 // import '../public/theme/dark/theme/index.css';
